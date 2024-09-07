c94fdee2835fed050004a5ff3ca1e8749eab9cd1
NguyenTruong <it10@katsuravn.com>
2024-09-07 14:52:40 +0700

    finish

--- frontend/mern-stack-notes-app/src/pages/SignUp/SignUp.jsx
+++ frontend/mern-stack-notes-app/src/pages/SignUp/SignUp.jsx
@@ -48,7 +48,7 @@ const SignUp = () => {
 
       if (response.data && response.data.accessToken) {
         localStorage.setItem('token', response.data.accessToken);
-        navigate('/dashboard');
+        navigate('/');
       }
     } catch (error) {
       if (error.response && error.response.data && error.response.data.message) {
