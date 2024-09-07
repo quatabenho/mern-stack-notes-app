9ef4f4e472a4356b2473bce3f95043044f2dde4a
NguyenTruong <it10@katsuravn.com>
2024-09-07 08:26:54 +0700

    feat: delete note API

--- backend/index.js
+++ backend/index.js
@@ -153,7 +153,6 @@ app.put("/edit-note/:noteId", authenticateToken, async(req, res) => {
     }
 });
 
-
 app.get("/get-all-notes", authenticateToken, async(req, res) => {
     const { user } = req.user;
 
@@ -172,6 +171,28 @@ app.get("/get-all-notes", authenticateToken, async(req, res) => {
     }
 });
 
+app.delete("/delete-note/:noteId", authenticateToken, async(req, res) => {
+    const noteId = req.params.noteId;
+    const { user } = req.user;
+
+    try {
+        const note = await Note.findOne({ _id: noteId, userId: user._id });
+        if(!note) {
+            return res.status(404).json({ message: "Note not found"});
+        }
+
+        await Note.deleteOne({ _id: noteId, userId: user._id });
+
+        return res.json({
+            error: false,
+            message: "Note deleted successfully",
+        });
+    } catch (error) {
+        return res.status(500).json({ 
+            error: true,
+            message: "Internal server error"});
+    }
+});
 app.listen(8000);
 
 module.exports = app;
