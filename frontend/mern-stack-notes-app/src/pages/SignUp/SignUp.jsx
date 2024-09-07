194e612afcb336841896c512fd07937e008f4316
NguyenTruong <it10@katsuravn.com>
2024-09-07 10:28:29 +0700

    feat: integrate sign up

--- frontend/mern-stack-notes-app/src/pages/SignUp/SignUp.jsx
+++ frontend/mern-stack-notes-app/src/pages/SignUp/SignUp.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import PasswordInput from '../../components/Input/PasswordInput'
 import { validateEmail } from '../../utils/helper'
-import { FaRegEye, FaRegEyeSlash } from 'react-icons/fa6'
-
+import axiosInstance from '../../utils/axiosInstance'
 
 const SignUp = () => {
 
@@ -12,6 +11,7 @@ const SignUp = () => {
   const [email, setEmail] = React.useState('')
   const [password, setPassword] = React.useState('')
   const [error, setError] = React.useState(null)
+  const navigate = useNavigate()
 
   const handleSignUp = async (e) => {
     e.preventDefault();
@@ -34,7 +34,29 @@ const SignUp = () => {
     setError('');
     
     // Sign Up API Call
+    try {
+      const response = await axiosInstance.post('/create-account', {
+        fullName: name,
+        email: email,
+        password: password,
+      });
 
+      if (response.data && response.data.error) {
+        setError(response.data.message);
+        return
+      }
+
+      if (response.data && response.data.accessToken) {
+        localStorage.setItem('token', response.data.accessToken);
+        navigate('/dashboard');
+      }
+    } catch (error) {
+      if (error.response && error.response.data && error.response.data.message) {
+        setError(error.response.data.message);
+      } else {
+        setError('Something went wrong');
+      }
+    }
   }
 
   return (
