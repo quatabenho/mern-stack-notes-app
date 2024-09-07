5c264b08dff1f62858f938accd38a85045e33c54
NguyenTruong <it10@katsuravn.com>
2024-09-07 13:23:07 +0700

    upd  empty page

--- frontend/mern-stack-notes-app/src/components/EmptyCard/EmptyCard.jsx
+++ frontend/mern-stack-notes-app/src/components/EmptyCard/EmptyCard.jsx
@@ -4,7 +4,7 @@ const EmptyCard = ({ imgSrc, message}) => {
   return (
     <div className='flex flex-col items-center justify-center mt-20'>
         <img src={imgSrc} alt="Empty notes" className='w-60' />
-        <p className='w-1/2 text-sm font-semibold text-slate-700 text-center leading-7 mt-5'>
+        <p className='w-1/2 text-xl font-semibold text-slate-700 text-center leading-7 mt-5'>
             {message}
         </p>
     </div>
--- frontend/mern-stack-notes-app/src/pages/Home/Home.jsx
+++ frontend/mern-stack-notes-app/src/pages/Home/Home.jsx
@@ -134,7 +134,7 @@ const Home = () => {
           ):(
           <EmptyCard
             imgSrc={AddNotesImg}
-            message={`Start creating your first note! Click the 'add' button to jot down your thoughts, ideas, and reminders.`}/>)}
+            message={`Start creating your first note!`}/>)}
         </div>
 
         <button className='w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10'
