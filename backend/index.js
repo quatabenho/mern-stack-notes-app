ddfc14e7a18e51bb7d4a6960e973bf3e8dc4ed62
NguyenTruong <it10@katsuravn.com>
2024-09-07 08:21:35 +0700

    feat: fetch notes API

--- backend/index.js
+++ backend/index.js
@@ -154,6 +154,24 @@ app.put("/edit-note/:noteId", authenticateToken, async(req, res) => {
 });
 
 
+app.get("/get-all-notes", authenticateToken, async(req, res) => {
+    const { user } = req.user;
+
+    try {
+        const notes = await Note.find({ userId: user._id }).sort({ isPinned: -1, createOn: -1 });
+
+        return res.json({
+            error: false,
+            notes,
+            message: "Notes fetched successfully",
+        });
+    } catch (error) {
+        return res.status(500).json({ 
+            error: true,
+            message: "Internal server error"});
+    }
+});
+
 app.listen(8000);
 
 module.exports = app;
