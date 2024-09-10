5300a440065662b21f7320b1be64c3112d661d84
NguyenTruong <it10@katsuravn.com>
2024-09-10 08:58:33 +0700

    hellen

--- frontend/mern-stack-notes-app/src/pages/Home/Home.jsx
+++ frontend/mern-stack-notes-app/src/pages/Home/Home.jsx
@@ -137,7 +137,7 @@ const Home = () => {
             imgSrc={AddNotesImg}
             message={`Start creating your first note!`}/>)}
         </div>
-        <img src={hellen} alt='Add Notes' className='w-1/2 mx-auto mt-10' />
+        <img src={hellen} alt='Add Notes' className='w-1/2 mx-auto '/>
 
         <button className='w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10'
          onClick={() => {
