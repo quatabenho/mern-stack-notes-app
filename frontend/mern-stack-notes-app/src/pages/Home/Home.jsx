389637ad1f32569a1cc555d16e7e297a1c5d4b30
NguyenTruong <it10@katsuravn.com>
2024-09-11 09:14:24 +0700

    add assets

--- frontend/mern-stack-notes-app/src/pages/Home/Home.jsx
+++ frontend/mern-stack-notes-app/src/pages/Home/Home.jsx
@@ -9,8 +9,6 @@ import axiosInstance from '../../utils/axiosInstance'
 import ToastMessage from '../../components/ToastMessage/ToastMessage'
 import EmptyCard from '../../components/EmptyCard/EmptyCard'
 import AddNotesImg from '../../../src/assets/addnote.svg'
-import hellen from '../../../src/assets/hellen.jpg'
-import sinhto from '../../../src/assets/sinhto.jpg'
 
 const Home = () => {
 
@@ -139,7 +137,7 @@ const Home = () => {
             imgSrc={AddNotesImg}
             message={`Start creating your first note!`}/>)}
         </div>
-             <img src={sinhto} alt="" srcset="" />
+
         <button className='w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10'
          onClick={() => {
           setOpenAddEditModal({ isShown: true, type: 'add', data: null })
